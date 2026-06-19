fa521127d37406cd8be417503fed70131fe0c10f
青木𝙰𝚘𝚔𝚒.𝚀𝙼 <3452798215@qq.com>
2026-06-20 05:34:46 +0800

    Fix branch to master

--- loader.lua
+++ loader.lua
@@ -1,3 +1,2 @@
 -- BFH Loader v1.0
--- 使用方法：在 Roblox 执行器中运行此脚本
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AokiQM/BFH-Encrypted-test/main/bundle.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AokiQM/BFH-Encrypted-test/master/bundle.lua"))()
